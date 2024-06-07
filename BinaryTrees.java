8a2243c05bba7f6abba0a9ea872022ab478e8da5
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-06-07 16:17:05 +0530

    Update BinaryTrees.java

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -26,3 +26,67 @@ public class BinaryTrees {
             return newNode;
         }
     }
+
+    //Preorder Traversal
+    public static void preOrder(Node root) {
+        if(root == null) {
+            //System.out.print(-1 + " ");
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    //Inorder Traversal
+    public static void inOrder(Node root) {
+        if(root == null) {
+            //System.out.print(-1 + " ");
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    //Postorder Traversal
+    public static void postOrder(Node root) {
+        if(root == null) {
+            //System.out.print(-1 + " ");
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    //Level Order Traversal
+    public static void levelOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()) {
+            Node curr = q.remove();
+            if(curr == null) {
+                System.out.println();
+                //Empty queue
+                if(q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(curr.data + " ");
+                if(curr.left != null) {
+                    q.add(curr.left);
+                }
+                if(curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
