0f80cce904f564bba5797fdadfa49595b0761fd1
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-06-07 16:16:13 +0530

    Create BinaryTrees.java

---
+++ BinaryTrees.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BinaryTrees {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+        public Node buildTree(int nodes[]) {
+            idx++;
+            if(nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
