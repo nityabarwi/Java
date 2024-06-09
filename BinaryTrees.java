1744221c4c8e529ffa54d95476fae3a921e3b47f
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-06-09 12:07:57 +0530

    Update BinaryTrees.java

--- BinaryTrees.java
+++ BinaryTrees.java
@@ -99,6 +99,15 @@ public class BinaryTrees {
             }
         }
     }
+    //Count total number of nodes
+    public static int countNodes(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftNodes = countNodes(root.left);
+        int rightNodes = countNodes(root.right);
+        return leftNodes + rightNodes + 1;
+    }
 
     public static void main(String [] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
