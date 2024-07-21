6b2efa72dd5f4e3b7004f4bddf62badf96663a8d
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-07-21 22:48:09 +0530

    Create Hashing2.java

---
+++ Hashing2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Hashing {
+    static class HashMap<K, V> {
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private int n;
+        private int N;
+        private LinkedList<Node> buckets[];
