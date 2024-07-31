34d9ca050a0e64280554905d45c28a6f2b69f8d2
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-07-31 22:08:39 +0530

    Update Tries.java

--- Tries.java
+++ Tries.java
@@ -1,1 +1,11 @@
+public class Tries {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow;
 
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
