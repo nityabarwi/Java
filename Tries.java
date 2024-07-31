5e7715f5a63610b002d86f9b6b5d885eecc7b8ab
NITYA BARWI <95531972+nityabarwi@users.noreply.github.com>
2024-07-31 22:09:14 +0530

    Update Tries.java

--- Tries.java
+++ Tries.java
@@ -9,3 +9,20 @@ public class Tries {
             }
         }
     }
+    public static Node root = new Node();
+
+    public static void insert(String word) { // O(n)
+        int level = 0;
+        int len = word.length();
+        int idx = 0;
+
+        Node curr = root;
+        for (; level < len; level++) {
+            idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
